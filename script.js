9e9599b2c82163b79016597449761f45fd847954
agk21 <Arun.Kanhai21@outlook.com>
2025-03-03 22:58:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function toggleTheme() {
     document.body.classList.toggle("dark-mode");
 }
 
+document.getElementById("email").addEventListener("click", function(event) {
+    window.location.href = "mailto:Arun.Kanhai55@qmail.cuny.edu";
+});
+
 // Attach the function to the button click event
 document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
 
