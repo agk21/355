e2809c083070a9659d96e7b443dc411431e5bb4c
agk21 <Arun.Kanhai21@outlook.com>
2025-05-04 22:02:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-// Toggle Dark Mode
-
-// Define the function separately
-function toggleTheme() {
-    document.body.classList.toggle("light-mode");
-    document.body.classList.toggle("dark-mode");
-}
-
-document.getElementById("email").addEventListener("click", function(event) {
-    window.location.href = "mailto:Arun.Kanhai55@qmail.cuny.edu";
+document.getElementById("theme-toggle").addEventListener("click", () => {
+  document.body.classList.toggle("light-mode");
+  document.body.classList.toggle("dark-mode");
 });
-
-// Attach the function to the button click event
-document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
-
